1731e28bce6a6e9eb4861b07d62553b289a95b03
kymiller <kylemiller457@gmail.com>
2025-03-19 21:10:30 -0500

    Adding simple metrics output to the example axum application about http requests

--- applications/axum-example/src/main.rs
+++ applications/axum-example/src/main.rs
@@ -1,9 +1,14 @@
 use anyhow::Result;
 use axum::{routing::get, Router};
+use axum_prometheus::PrometheusMetricLayer;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let app = Router::new().route("/hello", get(hello));
+    let (prometheus_layer, metric_handle) = PrometheusMetricLayer::pair();
+    let app = Router::new()
+        .route("/hello", get(hello))
+        .route("/metrics", get(|| async move { metric_handle.render() }))
+        .layer(prometheus_layer);
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await?;
     let _ = axum::serve(listener, app).await?;
