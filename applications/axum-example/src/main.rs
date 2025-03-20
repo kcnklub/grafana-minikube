648bc20ab42ed13f16259adb176d8b4af34018b0
kymiller <kylemiller457@gmail.com>
2025-03-20 16:21:11 -0500

    Adding loki deployment to the kubernetes spec files

--- applications/axum-example/src/main.rs
+++ applications/axum-example/src/main.rs
@@ -1,20 +1,46 @@
 use anyhow::Result;
 use axum::{routing::get, Router};
 use axum_prometheus::PrometheusMetricLayer;
+use std::process;
+use tower_http::trace::TraceLayer;
+use tracing_loki::url::Url;
+use tracing_subscriber::{prelude::__tracing_subscriber_SubscriberExt, util::SubscriberInitExt};
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    let (layer, task) = tracing_loki::builder()
+        .label("host", "mine")?
+        .extra_field("pid", format!("{}", process::id()))?
+        .build_url(Url::parse("http://127.0.0.1:3100").unwrap())?;
+
+    // We need to register our layer with `tracing`.
+    tracing_subscriber::registry()
+        .with(layer)
+        // One could add more layers here, for example logging to stdout:
+        // .with(tracing_subscriber::fmt::Layer::new())
+        .init();
+
+    tokio::spawn(task);
+
+    tracing::info!(
+        task = "tracing_setup",
+        result = "success",
+        "This is a new message for this morning",
+    );
+
     let (prometheus_layer, metric_handle) = PrometheusMetricLayer::pair();
     let app = Router::new()
         .route("/hello", get(hello))
         .route("/metrics", get(|| async move { metric_handle.render() }))
-        .layer(prometheus_layer);
+        .layer(prometheus_layer)
+        .layer(TraceLayer::new_for_http());
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await?;
     let _ = axum::serve(listener, app).await?;
     Ok(())
 }
 
+#[tracing::instrument(name = "hello!!!!")]
 async fn hello() -> String {
     "Hello from axum".into()
 }
--- request.py
+++ request.py
@@ -1,5 +1,5 @@
 import urllib.request
 
 while True:
-    contents = urllib.request.urlopen("http://localhost:9101/hello").read()
+    contents = urllib.request.urlopen("http://localhost:8080/hello").read()
     print(contents)
